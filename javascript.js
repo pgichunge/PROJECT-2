0573f043a7e44d5fc941d2b4037c75039e81f6ed
pamela mwendwa gichunge <pgichunge@safaricom.co.ke>
2021-12-08 01:29:24 +0300

    add changes done

--- javascript.js
+++ javascript.js
@@ -1,39 +1,17 @@
-function farLeft(CC) {
-    return ((CC / 4) - 2 * CC - 1);
-}
+// gather user input for
+const birthdaybox=document.getElementbyid("birthday")
+const birthmonthbox=document.getElementbyid("birthmonth")
+const birthyearbox=document.getElementbyid("birthyear")
+const birthgenderbox=document.getElementbyid("gender")
+const birthakannamebox=document.getElementbyid("akanname")
+ 
+//get the elements value
+const birthday=birthdaybox.value;
+const birthmonth=birthdaymonthbox.value;
+const birthyear=birthyearbox.value;
+const birthgender=birthgenderbox.value;
+const birthdayakanname=akannamebox.value;
 
-function middle(YY) {
-    return (5 * YY / 4);
-}
-
-function farRight(MM) {
-    return (26 * (MM + 1) / 10);
-}
-
-var birthdayYear = Number(prompt("Which year were you born?"))
-var gender = prompt("What is your gender?")
-
-const farLeftResult = farLeft(10)
-const farRightResult = farRight(10)
-const middleResult = middle(birthdayYear)
-
-var calculationResult = (farLeftResult + middleResult + farRightResult + 10) % 7
-const wholeNumber = calculationResult.toPrecision(1);
-alert(wholeNumber);
-
-// calculationResult = 2
-const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
-const femaleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
-
-let result;
-if (gender == "male") {
-    result = maleNames[wholeNumber]
-} else if (gender == "female") {
-    result = femaleNames[wholeNumber]
-} else {
-    alert('Enter a valid gender')
-}
-
-alert(`Your day name is ${result}`, )
+console.log(birthday,birthmonth,birthyear,birthgender,akanname)
 
  
