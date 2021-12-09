1d84dee7cc95240050a0ae96db5051105a300cfa
pamela mwendwa gichunge <pgichunge@safaricom.co.ke>
2021-12-09 13:49:15 +0300

    add changes done

--- javascript.js
+++ javascript.js
@@ -1,17 +1,63 @@
+//calculating the akanname which depends on the day of the week born
+// formula given  
+//Day of the week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
+function farLeft(CC) {
+    return ((CC / 4) - 2 * CC - 1);
+}
+
+function middle(YY) {
+    return (5 * YY / 4);
+}
+
+function farRight(MM) {
+    return (26 * (MM + 1) / 10);
+}
+
+var birthdayYear = Number(prompt("Which year were you born?"))
+var gender = prompt("What is your gender?")
+
+const farLeftResult = farLeft(10)
+const farRightResult = farRight(10)
+const middleResult = middle(birthdayYear)
+
+var calculationResult = (farLeftResult + middleResult + farRightResult + 10) % 7
+const wholeNumber = calculationResult.toPrecision(1);
+alert(wholeNumber);
+//calculatin results is 2
+const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
+const femaleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
+
+
+
+
 // gather user input for
 const birthdaybox=document.getElementbyid("birthday")
 const birthmonthbox=document.getElementbyid("birthmonth")
 const birthyearbox=document.getElementbyid("birthyear")
 const birthgenderbox=document.getElementbyid("gender")
-const birthakannamebox=document.getElementbyid("akanname")
+
  
 //get the elements value
 const birthday=birthdaybox.value;
 const birthmonth=birthdaymonthbox.value;
 const birthyear=birthyearbox.value;
 const birthgender=birthgenderbox.value;
-const birthdayakanname=akannamebox.value;
 
-console.log(birthday,birthmonth,birthyear,birthgender,akanname)
 
- 
+console.log(birthday,birthmonth,birthyear,birthgender,)
+
+//to define  the gender
+const allgendersarrays=["male","female"]
+if (allgendersarrays.includes[birthgender]){
+    console.log("continue here")
+//defining century and year
+
+const centuryvar=string (birthyear).substring[0,2]
+const yearvar=string (birthyear).substring[2]
+console.log(century,year)
+    
+}
+else{
+    alert("please enter either male or female in the genderbox")
+
+}
